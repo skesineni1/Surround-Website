c1a46b81be6b3d8fcd4a4a01207465050b9423a9
Krish <skesineni@deakin.edu.au>
2019-09-03 20:10:21 +1000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { FaGulp } from "react-icons/fa";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    
+  </Layout>
+);
+
+export default IndexPage;
