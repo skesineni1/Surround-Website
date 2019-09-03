a303859447c44809d85ed074bf7f8157535481a2
Krish <skesineni@deakin.edu.au>
2019-09-03 21:03:16 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import { FaGulp } from "react-icons/fa";
+
 
 const IndexPage = () => (
   <Layout>
