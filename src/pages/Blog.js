4967100ed494ab099f01498eb80528b3ce2dc5d5
Krish <skesineni@deakin.edu.au>
2019-09-06 09:33:03 +1000

    Create Blog.js

---
+++ src/pages/Blog.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+/*import Info from "../components/Home/Info.js";*/
+
+const BlogPage = ({data}) => (
+<Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        title= "Working on Blog" styleClass= "Blog-background"/>
+        
+    
+</Layout>
+);
+
+export const query = graphql`
+{
+img: file(relativePath: { eq: "managed-services.jpg" }) {
+    childImageSharp {
+        fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+        }
+    }
+    }
+}
+    `;
+
+
+export default BlogPage;
