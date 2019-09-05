dfe03803b0e2698fe7094c3cc66d9771ae419807
Krish <skesineni@deakin.edu.au>
2019-09-06 09:33:01 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection";
+import Info from "../components/Home/Info.js";
 
-
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <BackgroundSection
+        img={data.img.childImageSharp.fluid}
+        
+        styleClass="default-background"
+      />
+      <Info />
+      
     
   </Layout>
 );
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "Banner.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+    `;
+
 
 export default IndexPage;
