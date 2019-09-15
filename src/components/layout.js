594953815169f0d1c54292d53ff6eba04a2db433
Krish <skesineni@deakin.edu.au>
2019-09-15 15:56:12 +1000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,19 @@ import "./layout.css";
 
 import NavBar from "./Globals/Navbar";
 import Footer from "./Globals/Footer";
+import Services from "./Globals/Services";
 const Layout = ({ children }) => <>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"/>
+    <link rel="stylesheet" href="layout.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.4/dist/css/bulma-carousel.min.css"/>
 <NavBar/>
 {children}
-<Footer />
+<Services/>
+<Footer/>
 </>;
   
 
