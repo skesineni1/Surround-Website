02d8353a3420a2561c3f80663ff43df921a3bd7e
Krish <skesineni@deakin.edu.au>
2019-09-15 15:56:25 +1000

    Update Footer.js

--- src/components/Globals/Footer.js
+++ src/components/Globals/Footer.js
@@ -1,17 +1,46 @@
 import React from "react";
 
-export default function Footer() {
-return (
-    <footer className="footer py-3">
-    <div className="container">
-        <div className="row">
-        <div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
-            <h3>
-            All rights reserved &copy; {new Date().getFullYear().toString()}
-            </h3>
+const Footer = () => (
+    <div className = "site-footer">
+        <div class="container">
+        <div class="columns is-mobile is-multiline">
+        <div class="column is-haf-mobile is-one-quarter-desktop">
+            <h6 class="title is-5">Quick links</h6>
+            <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="team.html">Team</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            </ul>
+            </div>
+            <div class="column is-haf-mobile is-one-quarter-desktop">
+            <h6 class="title is-5">Information</h6>
+            <ul>
+            <li><a href="#0">Legal Information</a></li>
+            <li><a href="#0">Job oppurtunities</a></li>
+            <li><a href="#0">Frequently asked Questions</a></li>
+            </ul>
+            </div>
+            <div class="column is-haf-mobile is-one-quarter-desktop">
+            <h6 class="title is-5">Follow Us</h6>
+            <ul>
+            <li><a href="#0"><i class="fab fa-fw fa-Facebook"></i>Facebbok</a></li>
+            <li><a href="#0"><i class="fab fa-fw fa-Instagram"></i>Instagram</a></li>
+            <li><a href="#0"><i class="fab fa-fw fa-LinkedIn"></i>LinkedIn</a></li>
+            <li><a href="#0"><i class="fab fa-fw fa-Youtube"></i>Youtube</a></li>
+            </ul>
+            </div>
+            <div class="column is-haf-mobile is-one-quarter-desktop">
+            <h6 class="title is-5">Our Address</h6>
+            <li><a href="#0"> Deakin University, Building BC, Level 7, A2I2</a></li>
+            <i class="fas fa-fw fa-phone"></i>
+            <a href="tel:+1234567">(619) 234-0000</a>
+            </div>
         </div>
-        </div>
+        <p class="has-text-centered">&copy; surround.com | Designed by Deakin Students</p></div>
     </div>
-    </footer>
-);
-}
+
+)
+
+export default Footer
